549d1ff06b7d4ca4886e92ae1ab1adbb6e43c5e2
NickRuffini <njruffini@gmail.com>
2020-04-29 10:27:07 -0500

    Lab 36 Done

--- Ksu.Cis300.SortLibrary.Tests/SortTests.cs
+++ Ksu.Cis300.SortLibrary.Tests/SortTests.cs
@@ -1,1 +1,111 @@
-﻿// Unit tests go here.
+﻿/* SortTests.cs
+ * Author: Rod Howell
+ */
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Ksu.Cis300.SortLibrary.Tests
+{
+    /// <summary>
+    /// Unit tests for the Sort class.
+    /// </summary>
+    [TestFixture]
+    public class SortTests
+    {
+        /// <summary>
+        /// Tests SelectionSort on a 10-element array.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestASelectionSortSmall()
+        {
+            int[] data = new int[] { 3, -7, 0, 11, -7, 8, 4, 8, -2, 6 };
+            int[] result = new int[data.Length];
+            data.CopyTo(result, 0);
+            Sort.SelectionSort(result);
+            Assert.That(result, Is.Ordered.And.EquivalentTo(data));
+        }
+
+        /// <summary>
+        /// Iniitializes two lists to the same sequence of random ints.
+        /// </summary>
+        /// <param name="data">The first list.</param>
+        /// <param name="result">The second list.</param>
+        /// <param name="n">The number of elements in each list, and the upper bound
+        /// on their values (values will be nonnegative and less than n).</param>
+        private void Initialize(IList<int> data, IList<int> result, int n)
+        {
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                data.Add(r.Next(n));
+                result.Add(data[i]);
+            }
+        }
+
+        /// <summary>
+        /// Tests SelectionSort on a random list of 100 elements.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestBSelectionSort100()
+        {
+            List<int> data = new List<int>();
+            List<int> result = new List<int>();
+            Initialize(data, result, 100);
+            Sort.SelectionSort(result);
+            Assert.That(result, Is.Ordered.And.EquivalentTo(data));
+        }
+
+        [Test, Timeout(20000)]
+        /// <summary>
+        /// Tests SelectionSort on a random list of 30000 elements.
+        /// </summary>
+        public void TestCSelectionSort30000()
+        {
+            List<int> data = new List<int>();
+            List<int> result = new List<int>();
+            Initialize(data, result, 30000);
+            Sort.SelectionSort(result);
+            Assert.That(result, Is.Ordered);
+        }
+
+        /// <summary>
+        /// Tests InsertionSort on a 10-element array.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestDInsertionSortSmall()
+        {
+            int[] data = new int[] { 3, -7, 0, 11, -7, 8, 4, 8, -2, 6 };
+            int[] result = new int[data.Length];
+            data.CopyTo(result, 0);
+            Sort.InsertionSort(result);
+            Assert.That(result, Is.Ordered.And.EquivalentTo(data));
+        }
+
+        /// <summary>
+        /// Tests InsertionSort on a random list of 100 elements.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestEInsertionSort100()
+        {
+            List<int> data = new List<int>();
+            List<int> result = new List<int>();
+            Initialize(data, result, 100);
+            Sort.InsertionSort(result);
+            Assert.That(result, Is.Ordered.And.EquivalentTo(data));
+        }
+
+        [Test, Timeout(20000)]
+        /// <summary>
+        /// Tests InsertionSort on a random list of 30000 elements.
+        /// </summary>
+        public void TestFInsertionSort30000()
+        {
+            List<int> data = new List<int>();
+            List<int> result = new List<int>();
+            Initialize(data, result, 30000);
+            Sort.InsertionSort(result);
+            Assert.That(result, Is.Ordered);
+        }
+    }
+}
--- Ksu.Cis300.SortLibrary/Sort.cs
+++ Ksu.Cis300.SortLibrary/Sort.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Sort.cs
+ * Author: Nick Ruffini
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,67 @@ using System.Threading.Tasks;
 
 namespace Ksu.Cis300.SortLibrary
 {
+    /// <summary>
+    /// Class that contains both the insertion and selection sorting algorithms!
+    /// </summary>
     public static class Sort
     {
+        /// <summary>
+        /// Swaps the value at location i in list with the value in location j
+        /// </summary>
+        /// <param name="list"> List that we are swapping the two values in </param>
+        /// <param name="i"> Location of the first value we are swapping </param>
+        /// <param name="j"> Location of the second value we are swapping </param>
+        private static void Swap(IList<int> list, int i, int j)
+        {
+            int temp = list[j];
+            list[j] = list[i];
+            list[i] = temp;
+        }
+        /// <summary>
+        /// Selection sorting method that increments through a list, sorting from smallest to biggest
+        /// as it goes. 
+        /// </summary>
+        /// <param name="list"> List that we are applying selection sort to </param>
+        public static void SelectionSort(IList<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int minIndex = i; 
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[j] < list[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                Swap(list, minIndex, i);
+            }
+        }
+        /// <summary>
+        /// Insertion sorting method that takes the first element that is unsorted, and works it's way 
+        /// backwards through the sorted set!
+        /// </summary>
+        /// <param name="list"> List that we are applying insertion sort onto </param>
+        public static void InsertionSort(IList<int> list)
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                int cur = list[i];
+                int j;
+                for (j = i; j > 0; j--)
+                {
+                    if (list[j - 1] > cur)
+                    {
+                        list[j] = list[j - 1];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                list[j] = cur;
+            }
+        }
     }
 }
